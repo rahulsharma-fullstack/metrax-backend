b0f5ff64c0953138cf10aeb012a6abcf4312a5df
rahulsharma-fullstack <itsmerahul.contact@gmail.com>
2025-07-23 14:18:15 -0400

    email notification v1.3

--- services/resendEmailService.js
+++ services/resendEmailService.js
@@ -19,8 +19,8 @@ class ResendEmailService {
         ? 'Metrax Website <noreply@mail.metraxindigenous.com>' 
         : 'Metrax Website <onboarding@resend.dev>',
       adminEmail: isProduction
-        ? ['h.logsend@metraxindigenous.com']
-        : ['h.logsend@metraxindigenous.com']
+        ? ['r.sharma@metraxindigenous.com']
+        : ['r.sharma@metraxindigenous.com']
     };
   }
   async sendContactNotification(contactData) {
